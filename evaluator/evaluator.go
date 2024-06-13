dca07fb313376da55b9c672e390476488b209fa0
MBATheGamer <mbathegamer@gmail.com>
2024-06-13 17:28:28 +0100

    create `Eval` function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -1,1 +1,14 @@
 package evaluator
+
+import (
+	"github.com/MBATheGamer/lang_core/ast"
+	"github.com/MBATheGamer/lang_core/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
