a731a173fa42f286469ab979213687abcf7ddca5
MBATheGamer <mbathegamer@gmail.com>
2024-06-13 17:35:04 +0100

    completing `Eval` function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,8 +7,15 @@ import (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
 	}
 	return nil
 }
