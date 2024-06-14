3e0ef35e6353f52c7a4e578ad9886bf18fd70240
MBATheGamer <mbathegamer@gmail.com>
2024-06-14 17:45:49 +0100

    create `testIntegerObject` function

--- evaluator/test/evaluator_test.go
+++ evaluator/test/evaluator_test.go
@@ -1,6 +1,8 @@
 package evaluator_test
 
 import (
+	"testing"
+
 	"github.com/MBATheGamer/lang_core/lexer"
 	"github.com/MBATheGamer/lang_core/object"
 	"github.com/MBATheGamer/lang_core/parser"
@@ -13,4 +15,20 @@ func testEval(input string) object.Object {
 	var program = parser.ParseProgram()
 
 	return evaluator.Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	var result, ok = obj.(*object.Integer)
+
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
 }
