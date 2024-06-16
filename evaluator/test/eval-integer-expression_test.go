62d2db57e863dafd96f6948f096d4cb467e72a6a
MBATheGamer <mbathegamer@gmail.com>
2024-06-16 18:19:48 +0100

    Refactor: `TestEvalIntegerExpression` function

--- evaluator/test/eval-integer-expression_test.go
+++ evaluator/test/eval-integer-expression_test.go
@@ -1,6 +1,15 @@
 package evaluator_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/MBATheGamer/lang_core/object"
+)
+
+type TestIntegerExpression struct {
+	input    string
+	expected int64
+}
 
 func TestEvalIntegerExpression(t *testing.T) {
 	var tests = []TestIntegerExpression{
@@ -12,4 +21,20 @@ func TestEvalIntegerExpression(t *testing.T) {
 		var evaluated = testEval(test.input)
 		testIntegerObject(t, evaluated, test.expected)
 	}
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	var result, ok = obj.(*object.Integer)
+
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
 }
--- evaluator/test/evaluator_test.go
+++ evaluator/test/evaluator_test.go
@@ -1,18 +1,11 @@
 package evaluator_test
 
 import (
-	"testing"
-
 	"github.com/MBATheGamer/lang_core/lexer"
 	"github.com/MBATheGamer/lang_core/object"
 	"github.com/MBATheGamer/lang_core/parser"
 	"github.com/MBATheGamer/repl/evaluator"
 )
-
-type TestIntegerExpression struct {
-	input    string
-	expected int64
-}
 
 func testEval(input string) object.Object {
 	var lexer = lexer.New(input)
@@ -20,20 +13,4 @@ func testEval(input string) object.Object {
 	var program = parser.ParseProgram()
 
 	return evaluator.Eval(program)
-}
-
-func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
-	var result, ok = obj.(*object.Integer)
-
-	if !ok {
-		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
-		return false
-	}
-
-	if result.Value != expected {
-		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
-		return false
-	}
-
-	return true
 }
