b202290dce3fa8a760019fafc3f3e8452d62d711
MBATheGamer <mbathegamer@gmail.com>
2024-06-21 17:42:08 +0100

    create `evalPrefixExpression` function

---
+++ evaluator/eval-prefix-expression.go
@@ -0,0 +1,12 @@
+package evaluator
+
+import "github.com/MBATheGamer/lang_core/object"
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
