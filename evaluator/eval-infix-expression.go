39d29933b414c7018ac38851d324c48d292d8e51
MBATheGamer <mbathegamer@gmail.com>
2024-06-25 02:18:56 +0100

    create `evalInfixExpression` function

---
+++ evaluator/eval-infix-expression.go
@@ -0,0 +1,13 @@
+package evaluator
+
+import "github.com/MBATheGamer/lang_core/object"
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+
+	default:
+		return NULL
+	}
+}
