dc606ea83fdd6c753c3cd827ed27ea6f90711b48
MBATheGamer <mbathegamer@gmail.com>
2024-06-30 13:39:56 +0100

    Create `isTruthy` function

---
+++ evaluator/is-truthy.go
@@ -0,0 +1,16 @@
+package evaluator
+
+import "github.com/MBATheGamer/lang_core/object"
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
+}
