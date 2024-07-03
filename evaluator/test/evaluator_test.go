6de4ee7d4d9559d4c780e753bdc54ab68a8b4e64
MBATheGamer <mbathegamer@gmail.com>
2024-07-03 13:57:09 +0100

    Create `testNullObject` function

--- evaluator/test/evaluator_test.go
+++ evaluator/test/evaluator_test.go
@@ -1,6 +1,8 @@
 package evaluator_test
 
 import (
+	"testing"
+
 	"github.com/MBATheGamer/lang_core/lexer"
 	"github.com/MBATheGamer/lang_core/object"
 	"github.com/MBATheGamer/lang_core/parser"
@@ -13,4 +15,17 @@ func testEval(input string) object.Object {
 	var program = parser.ParseProgram()
 
 	return evaluator.Eval(program)
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != evaluator.NULL {
+		t.Errorf(
+			"object is not NULL. got=%T (%+v)",
+			obj,
+			obj,
+		)
+		return false
+	}
+
+	return true
 }
