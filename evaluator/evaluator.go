8b7c2efdabca6dd95bb8c0e8cf19ac78de3ecc3e
MBATheGamer <mbathegamer@gmail.com>
2024-07-15 22:27:07 +0100

    Add `isError` to `PrefixExpression` case

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -34,6 +34,9 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.PrefixExpression:
 		var right = Eval(node.Right)
+		if isError(right) {
+			return right
+		}
 		return evalPrefixExpression(node.Operator, right)
 
 	case *ast.InfixExpression:
