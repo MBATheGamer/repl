ebf0cacee76cb619c65831cfb40cc150f62151df
MBATheGamer <mbathegamer@gmail.com>
2024-07-22 22:38:34 +0100

    Add `environment` to `evalIfExpression` function

--- evaluator/eval-if-expression.go
+++ evaluator/eval-if-expression.go
@@ -5,17 +5,17 @@ import (
 	"github.com/MBATheGamer/lang_core/object"
 )
 
-func evalIfExpression(ifExpression *ast.IfExpression) object.Object {
-	var condition = Eval(ifExpression.Condition)
+func evalIfExpression(ifExpression *ast.IfExpression, environment *object.Enivronment) object.Object {
+	var condition = Eval(ifExpression.Condition, environment)
 
 	if isError(condition) {
 		return condition
 	}
 
 	if isTruthy(condition) {
-		return Eval(ifExpression.Consequence)
+		return Eval(ifExpression.Consequence, environment)
 	} else if ifExpression.Alternative != nil {
-		return Eval(ifExpression.Alternative)
+		return Eval(ifExpression.Alternative, environment)
 	} else {
 		return NULL
 	}
