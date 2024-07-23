10ebcbc13252bd12b7911446e9e875c1fc828ba4
MBATheGamer <mbathegamer@gmail.com>
2024-07-23 22:38:28 +0100

    Add `environment` to `evalProgram` function

--- evaluator/eval-program.go
+++ evaluator/eval-program.go
@@ -5,11 +5,11 @@ import (
 	"github.com/MBATheGamer/lang_core/object"
 )
 
-func evalProgram(program *ast.Program) object.Object {
+func evalProgram(program *ast.Program, environment *object.Enivronment) object.Object {
 	var result object.Object
 
 	for _, statement := range program.Statements {
-		result = Eval(statement)
+		result = Eval(statement, environment)
 
 		switch result := result.(type) {
 		case *object.ReturnValue:
