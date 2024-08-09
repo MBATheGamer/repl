4916ddde388480092a61b080e6b44bf048a3518c
MBATheGamer <mbathegamer@gmail.com>
2024-08-09 03:18:05 +0100

    Create `TestEvalStringConcatenation` function

---
+++ evaluator/test/eval-string-concatenation_test.go
@@ -0,0 +1,29 @@
+package evaluator_test
+
+import (
+	"testing"
+
+	"github.com/MBATheGamer/lang_core/object"
+)
+
+func TestEvalStringConcatenation(t *testing.T) {
+	var input = `"Hello," + " " + "World!"`
+
+	var evaluated = testEval(input)
+	var str, ok = evaluated.(*object.String)
+
+	if !ok {
+		t.Fatalf(
+			"object is not String. got=%T (%+v)",
+			evaluated,
+			evaluated,
+		)
+	}
+
+	if str.Value != "Hello, World!" {
+		t.Errorf(
+			"String has wrong value. got=%q",
+			str.Value,
+		)
+	}
+}
