e2ccf52be82f3a16544ce9939f78be900da2126e
MBATheGamer <mbathegamer@gmail.com>
2024-08-12 03:21:01 +0100

    Add `len` key to `builtins` HashMap

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -4,4 +4,10 @@ import (
 	"github.com/MBATheGamer/lang_core/object"
 )
 
-var builtins = map[string]*object.Builtin{}
+var builtins = map[string]*object.Builtin{
+	"len": {
+		Fn: func(arguments ...object.Object) object.Object {
+			return NULL
+		},
+	},
+}
