2e8673cf2a6d52e4af64fcf36e63e858a78442eb
MBATheGamer <mbathegamer@gmail.com>
2024-08-14 03:24:01 +0100

    Add `len` logic in `builtins` HashMap

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -7,7 +7,25 @@ import (
 var builtins = map[string]*object.Builtin{
 	"len": {
 		Fn: func(arguments ...object.Object) object.Object {
-			return NULL
+			if len(arguments) != 1 {
+				return newError(
+					"wrong number of arguments. got=%d, want=1",
+					len(arguments),
+				)
+			}
+
+			switch argument := arguments[0].(type) {
+			case *object.String:
+				return &object.Integer{
+					Value: int64(len(argument.Value)),
+				}
+
+			default:
+				return newError(
+					"argument to `len` not supported, got %s",
+					arguments[0].Type(),
+				)
+			}
 		},
 	},
 }
