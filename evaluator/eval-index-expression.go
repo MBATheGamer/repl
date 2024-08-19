780437ca13eca046ed00d6edeaadc755fd8bc9b2
MBATheGamer <mbathegamer@gmail.com>
2024-08-19 03:26:56 +0100

    Create `evalIndexExpression` function

---
+++ evaluator/eval-index-expression.go
@@ -0,0 +1,16 @@
+package evaluator
+
+import "github.com/MBATheGamer/lang_core/object"
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+
+	default:
+		return newError(
+			"index operator not supported: %s",
+			left.Type(),
+		)
+	}
+}
