376531652f0bd48ccc00c120bdf62e90df95c3f7
MBATheGamer <mbathegamer@gmail.com>
2024-08-20 03:27:38 +0100

    Add `IndexExpression` case to `Eval` function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -93,6 +93,17 @@ func Eval(node ast.Node, environment *object.Enivronment) object.Object {
 		}
 		return &object.Array{Elements: elements}
 
+	case *ast.IndexExpression:
+		var left = Eval(node.Left, environment)
+		if isError(left) {
+			return left
+		}
+		var index = Eval(node.Index, environment)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
+
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
 	}
