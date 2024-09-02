0f43b1459da17021f7cf6d332862ab60039e1cab
MBATheGamer <mbathegamer@gmail.com>
2024-09-02 03:36:40 +0100

    Create `TestHashIndexExpression` function

---
+++ evaluator/test/eval-hash-index-expression_test.go
@@ -0,0 +1,52 @@
+package evaluator_test
+
+import "testing"
+
+type HashIndexExpressionType struct {
+	input    string
+	expected interface{}
+}
+
+func TestEvalHashIndexExpression(t *testing.T) {
+	var tests = []HashIndexExpressionType{
+		{
+			`{"foo": 5}["foo"]`,
+			5,
+		},
+		{
+			`{"foo": 5}["bar"]`,
+			nil,
+		},
+		{
+			`let key  = "foo"; {"foo": 5}[key]`,
+			5,
+		},
+		{
+			`{}["foo"]`,
+			nil,
+		},
+		{
+			`{5: 5}[5]`,
+			5,
+		},
+		{
+			`{true: 5}[true]`,
+			5,
+		},
+		{
+			`{false: 5}[false]`,
+			5,
+		},
+	}
+
+	for _, test := range tests {
+		var evaluated = testEval(test.input)
+		var integer, ok = test.expected.(int)
+
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
